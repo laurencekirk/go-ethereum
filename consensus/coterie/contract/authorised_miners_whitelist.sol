234b40ae5cb126a189e870da857d3b115312f7b5
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2017-11-29 11:39:23 +0000

    Improving the whitelist smart contract

--- consensus/coterie/contract/authorised_miners_whitelist.sol
+++ consensus/coterie/contract/authorised_miners_whitelist.sol
@@ -1,18 +1,28 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.18;
 
 contract AuthorisedMinersWhitelist {
 
     mapping(address => bool) whitelist;
+    uint32 public size;
+
+    event AddedToWhitelist(address miner);
+    event RemovedFromWhitelist(address miner);
 
     function isAuthorisedMiner(address miner) public view returns (bool) {
         return whitelist[miner];
     }
 
     function authoriseMiner(address miner) public {
+        require(! whitelist[miner]);
         whitelist[miner] = true;
+        size = size + 1;
+        AddedToWhitelist(miner);
     }
 
     function removeMinersAuthorisation(address miner) public {
+        require(whitelist[miner]);
         whitelist[miner] = false;
+        size = size - 1;
+        RemovedFromWhitelist(miner);
     }
 }
