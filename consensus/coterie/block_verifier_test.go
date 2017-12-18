ad8afcea1696bfd6752f922c7781a4ba6ae4b7e1
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2017-12-18 16:37:39 +0000

    #42 Adding tests for the validation logic

--- consensus/coterie/block_verifier_test.go
+++ consensus/coterie/block_verifier_test.go
@@ -3,7 +3,181 @@ package coterie
 import (
 	"testing"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/common"
+	"math/big"
 )
+
+/**
+ * RetrieveBlockAuthor tests START
+ */
+
+ // To verify authorisation signature We require the seed from the parent's header and the block number - and the address that we expect to have authorised the block.
+func TestRetrieveBlockAuthorValidSignature(t *testing.T) {
+	cases := []struct {
+		authorisation, parentSeed, addressExpected string
+		blockNumber int64
+	}{
+		{
+			"0xd817f3ba3956cbccd769afd17ef2b08a8ab7997d52cf5db55974c38c28fa24b90b814957b864a5adcca553cba2157574c39a7c10c342cb86c09d9603cdf037e200",
+			"0x8c68c72f23975111ed783d3351730266489971f0fea49d43196ab7b482ce6fb6648d2f50dcfc5619c0dfa3f023882de27ee0f01fb98bc9c124c47cb11f757fe300",
+			"0x30ff130a7d11ef9d1efbdf19d5309556acd129cf",
+			2,
+		},
+		{
+			"0x0860c7eeeaa9070d7c3b1116122671171846e2b5e8a29ae3a0b9b7637e281f173a30a148ba876379723c13a7de4a948645dd39b1b66e705fc2c5fe3eb7aa8a1301",
+			"0xc723f849846dfb865134a28b7bb2e9b19b2c454ee8945ea41641e6809bb710da380da3edeecc0bde2a52e6245f3e2841bbdbaab95a6cb9828d20861d02c7f68b00",
+			"0xea30250dd7263a4783c66463c236a2153d6b88b4",
+			42,
+			},
+		{
+			"0x1f31fb2878cf6cbefbecad4b05664a2af87d5ec725cfe9e6a2123bdd16cb1ece134a3647dd52d72d2ea4c010454d22fe39e5c875f666e33dfa33dabcf5bad5aa01",
+			"0x884a3ad3e55ea7b9917b07128dc58c544d7a27611336e9ee445219fbadd413943677b96221c39a708c55fb4deaa0a80c3d12f9bf1d059028e958d73e31522b0101",
+			"0x6c80e492308f051eba48d03bcc04625682ae3e07",
+			100,
+		},
+	}
+	for _, c := range cases {
+		// Set up
+		parentHeader := getMockedParentHeader()
+		currentBlockHeader := getMockedBlockHeader()
+
+		parentHeader.ExtendedHeader.Seed = *types.HexToSignature(c.parentSeed)
+
+		currentBlockHeader.Number = big.NewInt(c.blockNumber)
+		currentBlockHeader.ExtendedHeader.Authorisation = *types.HexToSignature(c.authorisation)
+
+		expectedAddress := common.HexToAddress(c.addressExpected)
+
+		// Test
+		retrievedAddress, err := RetrieveBlockAuthor(parentHeader, currentBlockHeader)
+
+		// Verify
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if expectedAddress != retrievedAddress {
+			t.Errorf("Retrieve address does not matched the expected address: expected %v, got %v", expectedAddress.String(), retrievedAddress.String())
+		}
+	}
+}
+
+/*
+ * Scenario:
+ * A valid signature has been taken from block Br, but the subsequently propagated block at the same block-height (r) as Br has been modified to point to a
+ * different parent e.g. not Br-1.
+ * The retrieved address should, therefore, not be that of the original authoriser
+ */
+func TestRetrieveBlockAuthorInvalidParentSeedValidBlockNumberIsNotValidated(t *testing.T) {
+	cases := []struct {
+		authorisation, parentSeed, addressExpected string
+		blockNumber int64
+	}{
+		{
+			"0xd817f3ba3956cbccd769afd17ef2b08a8ab7997d52cf5db55974c38c28fa24b90b814957b864a5adcca553cba2157574c39a7c10c342cb86c09d9603cdf037e200",
+			"0xc723f849846dfb865134a28b7bb2e9b19b2c454ee8945ea41641e6809bb710da380da3edeecc0bde2a52e6245f3e2841bbdbaab95a6cb9828d20861d02c7f68b00",
+			"0x30ff130a7d11ef9d1efbdf19d5309556acd129cf",
+			2,
+		},
+		{
+			"0x0860c7eeeaa9070d7c3b1116122671171846e2b5e8a29ae3a0b9b7637e281f173a30a148ba876379723c13a7de4a948645dd39b1b66e705fc2c5fe3eb7aa8a1301",
+			"0x884a3ad3e55ea7b9917b07128dc58c544d7a27611336e9ee445219fbadd413943677b96221c39a708c55fb4deaa0a80c3d12f9bf1d059028e958d73e31522b0101",
+			"0xea30250dd7263a4783c66463c236a2153d6b88b4",
+			42,
+		},
+		{
+			"0x1f31fb2878cf6cbefbecad4b05664a2af87d5ec725cfe9e6a2123bdd16cb1ece134a3647dd52d72d2ea4c010454d22fe39e5c875f666e33dfa33dabcf5bad5aa01",
+			"0x8c68c72f23975111ed783d3351730266489971f0fea49d43196ab7b482ce6fb6648d2f50dcfc5619c0dfa3f023882de27ee0f01fb98bc9c124c47cb11f757fe300",
+			"0x6c80e492308f051eba48d03bcc04625682ae3e07",
+			100,
+		},
+	}
+	for _, c := range cases {
+		// Set up
+		parentHeader := getMockedParentHeader()
+		currentBlockHeader := getMockedBlockHeader()
+
+		parentHeader.ExtendedHeader.Seed = *types.HexToSignature(c.parentSeed)
+
+		currentBlockHeader.Number = big.NewInt(c.blockNumber)
+		currentBlockHeader.ExtendedHeader.Authorisation = *types.HexToSignature(c.authorisation)
+
+		expectedAddress := common.HexToAddress(c.addressExpected)
+
+		// Test
+		retrievedAddress, err := RetrieveBlockAuthor(parentHeader, currentBlockHeader)
+
+		// Verify
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if expectedAddress == retrievedAddress {
+			t.Errorf("Retrieve address does not matched the expected address: expected %v, got %v", expectedAddress.String(), retrievedAddress.String())
+		}
+	}
+}
+
+/*
+ * Scenario:
+ * In the unlikely situation that a valid seed is produced in more than one block (say that the seed Sa was first seen in Block Sa),
+ * then we want to make sure that re-using the authorisation signature (Aa) in block (Br) doesn't result in the consensus layer thinking that the
+ * authorised account has produced block Br.
+ * The retrieved address should, therefore, not be that of the original authoriser
+ */
+func TestRetrieveBlockAuthorValidParentSeedInvalidBlockNumberIsNotValidated(t *testing.T) {
+	cases := []struct {
+		authorisation, parentSeed, addressExpected string
+		blockNumber int64
+	}{
+		{
+			"0xd817f3ba3956cbccd769afd17ef2b08a8ab7997d52cf5db55974c38c28fa24b90b814957b864a5adcca553cba2157574c39a7c10c342cb86c09d9603cdf037e200",
+			"0x8c68c72f23975111ed783d3351730266489971f0fea49d43196ab7b482ce6fb6648d2f50dcfc5619c0dfa3f023882de27ee0f01fb98bc9c124c47cb11f757fe300",
+			"0x30ff130a7d11ef9d1efbdf19d5309556acd129cf",
+			3,
+		},
+		{
+			"0x0860c7eeeaa9070d7c3b1116122671171846e2b5e8a29ae3a0b9b7637e281f173a30a148ba876379723c13a7de4a948645dd39b1b66e705fc2c5fe3eb7aa8a1301",
+			"0xc723f849846dfb865134a28b7bb2e9b19b2c454ee8945ea41641e6809bb710da380da3edeecc0bde2a52e6245f3e2841bbdbaab95a6cb9828d20861d02c7f68b00",
+			"0xea30250dd7263a4783c66463c236a2153d6b88b4",
+			41,
+		},
+		{
+			"0x1f31fb2878cf6cbefbecad4b05664a2af87d5ec725cfe9e6a2123bdd16cb1ece134a3647dd52d72d2ea4c010454d22fe39e5c875f666e33dfa33dabcf5bad5aa01",
+			"0x884a3ad3e55ea7b9917b07128dc58c544d7a27611336e9ee445219fbadd413943677b96221c39a708c55fb4deaa0a80c3d12f9bf1d059028e958d73e31522b0101",
+			"0x6c80e492308f051eba48d03bcc04625682ae3e07",
+			9001,
+		},
+	}
+	for _, c := range cases {
+		// Set up
+		parentHeader := getMockedParentHeader()
+		currentBlockHeader := getMockedBlockHeader()
+
+		parentHeader.ExtendedHeader.Seed = *types.HexToSignature(c.parentSeed)
+
+		currentBlockHeader.Number = big.NewInt(c.blockNumber)
+		currentBlockHeader.ExtendedHeader.Authorisation = *types.HexToSignature(c.authorisation)
+
+		expectedAddress := common.HexToAddress(c.addressExpected)
+
+		// Test
+		retrievedAddress, err := RetrieveBlockAuthor(parentHeader, currentBlockHeader)
+
+		// Verify
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if expectedAddress == retrievedAddress {
+			t.Errorf("Retrieve address does not matched the expected address: expected %v, got %v", expectedAddress.String(), retrievedAddress.String())
+		}
+	}
+}
+
+/**
+ * RetrieveBlockAuthor tests END
+ */
 
 /*
  * isSeedValid Tests Start
