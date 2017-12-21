6df41626e8783165a7eba018cf2daa1cdd37c74e
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2017-12-21 10:01:02 +0000

    #50 Initial steps towards including the test coverage of the consensus layer

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+//go:generate mockgen -source=consensus.go -destination=coterie/mocks/mock_consensus.go -package=mocks
+
 // Package consensus implements different Ethereum consensus engines.
 package consensus
 
