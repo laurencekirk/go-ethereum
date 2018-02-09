d2466ddf25edc19c1f353d7666dffff40bf0f7d5
Stephen Jackson <stevejaxon@users.noreply.github.com>
2018-02-09 17:23:29 +0000

    Update azure.go

--- internal/build/azure.go
+++ internal/build/azure.go
@@ -17,95 +17,117 @@
 package build
 
 import (
+	"flag"
 	"fmt"
 	"os"
-
-	storage "github.com/Azure/azure-storage-go"
+	"strings"
 )
 
-// AzureBlobstoreConfig is an authentication and configuration struct containing
-// the data needed by the Azure SDK to interact with a speicifc container in the
-// blobstore.
-type AzureBlobstoreConfig struct {
-	Account   string // Account name to authorize API requests with
-	Token     string // Access token for the above account
-	Container string // Blob container to upload files into
+var (
+	// These flags override values in build env.
+	GitCommitFlag   = flag.String("git-commit", "", `Overrides git commit hash embedded into executables`)
+	GitBranchFlag   = flag.String("git-branch", "", `Overrides git branch being built`)
+	GitTagFlag      = flag.String("git-tag", "", `Overrides git tag being built`)
+	BuildnumFlag    = flag.String("buildnum", "", `Overrides CI build number`)
+	PullRequestFlag = flag.Bool("pull-request", false, `Overrides pull request status of the build`)
+	CronJobFlag     = flag.Bool("cron-job", false, `Overrides cron job status of the build`)
+)
+
+// Environment contains metadata provided by the build environment.
+type Environment struct {
+	Name                string // name of the environment
+	Repo                string // name of GitHub repo
+	Commit, Branch, Tag string // Git info
+	Buildnum            string
+	IsPullRequest       bool
+	IsCronJob           bool
 }
 
-// AzureBlobstoreUpload uploads a local file to the Azure Blob Storage. Note, this
-// method assumes a max file size of 64MB (Azure limitation). Larger files will
-// need a multi API call approach implemented.
-//
-// See: https://msdn.microsoft.com/en-us/library/azure/dd179451.aspx#Anchor_3
-func AzureBlobstoreUpload(path string, name string, config AzureBlobstoreConfig) error {
-	if *DryRunFlag {
-		fmt.Printf("would upload %q to %s/%s/%s\n", path, config.Account, config.Container, name)
-		return nil
-	}
-	// Create an authenticated client against the Azure cloud
-	rawClient, err := storage.NewBasicClient(config.Account, config.Token)
-	if err != nil {
-		return err
-	}
-	client := rawClient.GetBlobService()
-
-	// Stream the file to upload into the designated blobstore container
-	in, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-
-	info, err := in.Stat()
-	if err != nil {
-		return err
-	}
-	return client.CreateBlockBlobFromReader(config.Container, name, uint64(info.Size()), in, nil)
+func (env Environment) String() string {
+	return fmt.Sprintf("%s env (commit:%s branch:%s tag:%s buildnum:%s pr:%t)",
+		env.Name, env.Commit, env.Branch, env.Tag, env.Buildnum, env.IsPullRequest)
 }
 
-// AzureBlobstoreList lists all the files contained within an azure blobstore.
-func AzureBlobstoreList(config AzureBlobstoreConfig) ([]storage.Blob, error) {
-	// Create an authenticated client against the Azure cloud
-	rawClient, err := storage.NewBasicClient(config.Account, config.Token)
-	if err != nil {
-		return nil, err
-	}
-	client := rawClient.GetBlobService()
-
-	// List all the blobs from the container and return them
-	container := client.GetContainerReference(config.Container)
-
-	blobs, err := container.ListBlobs(storage.ListBlobsParameters{
-		MaxResults: 1024 * 1024 * 1024, // Yes, fetch all of them
-		Timeout:    3600,               // Yes, wait for all of them
-	})
-	if err != nil {
-		return nil, err
-	}
-	return blobs.Blobs, nil
-}
-
-// AzureBlobstoreDelete iterates over a list of files to delete and removes them
-// from the blobstore.
-func AzureBlobstoreDelete(config AzureBlobstoreConfig, blobs []storage.Blob) error {
-	if *DryRunFlag {
-		for _, blob := range blobs {
-			fmt.Printf("would delete %s (%s) from %s/%s\n", blob.Name, blob.Properties.LastModified, config.Account, config.Container)
+// Env returns metadata about the current CI environment, falling back to LocalEnv
+// if not running on CI.
+func Env() Environment {
+	switch {
+	case os.Getenv("CI") == "true" && os.Getenv("TRAVIS") == "true":
+		return Environment{
+			Name:          "travis",
+			Repo:          os.Getenv("TRAVIS_REPO_SLUG"),
+			Commit:        os.Getenv("TRAVIS_COMMIT"),
+			Branch:        os.Getenv("TRAVIS_BRANCH"),
+			Tag:           os.Getenv("TRAVIS_TAG"),
+			Buildnum:      os.Getenv("TRAVIS_BUILD_NUMBER"),
+			IsPullRequest: os.Getenv("TRAVIS_PULL_REQUEST") != "false",
+			IsCronJob:     os.Getenv("TRAVIS_EVENT_TYPE") == "cron",
 		}
-		return nil
+	case os.Getenv("CI") == "True" && os.Getenv("APPVEYOR") == "True":
+		return Environment{
+			Name:          "appveyor",
+			Repo:          os.Getenv("APPVEYOR_REPO_NAME"),
+			Commit:        os.Getenv("APPVEYOR_REPO_COMMIT"),
+			Branch:        os.Getenv("APPVEYOR_REPO_BRANCH"),
+			Tag:           os.Getenv("APPVEYOR_REPO_TAG_NAME"),
+			Buildnum:      os.Getenv("APPVEYOR_BUILD_NUMBER"),
+			IsPullRequest: os.Getenv("APPVEYOR_PULL_REQUEST_NUMBER") != "",
+			IsCronJob:     os.Getenv("APPVEYOR_SCHEDULED_BUILD") == "True",
+		}
+	default:
+		return LocalEnv()
 	}
-	// Create an authenticated client against the Azure cloud
-	rawClient, err := storage.NewBasicClient(config.Account, config.Token)
-	if err != nil {
-		return err
-	}
-	client := rawClient.GetBlobService()
+}
 
-	// Iterate over the blobs and delete them
-	for _, blob := range blobs {
-		if err := client.DeleteBlob(config.Container, blob.Name, nil); err != nil {
-			return err
+// LocalEnv returns build environment metadata gathered from git.
+func LocalEnv() Environment {
+	env := applyEnvFlags(Environment{Name: "local", Repo: "ethereum/go-ethereum"})
+
+	head := readGitFile("HEAD")
+	if splits := strings.Split(head, " "); len(splits) == 2 {
+		head = splits[1]
+	} else {
+		return env
+	}
+	if env.Commit == "" {
+		env.Commit = readGitFile(head)
+	}
+	if env.Branch == "" {
+		if head != "HEAD" {
+			env.Branch = strings.TrimLeft(head, "refs/heads/")
 		}
 	}
-	return nil
+	if info, err := os.Stat(".git/objects"); err == nil && info.IsDir() && env.Tag == "" {
+		env.Tag = firstLine(RunGit("tag", "-l", "--points-at", "HEAD"))
+	}
+	return env
+}
+
+func firstLine(s string) string {
+	return strings.Split(s, "\n")[0]
+}
+
+func applyEnvFlags(env Environment) Environment {
+	if !flag.Parsed() {
+		panic("you need to call flag.Parse before Env or LocalEnv")
+	}
+	if *GitCommitFlag != "" {
+		env.Commit = *GitCommitFlag
+	}
+	if *GitBranchFlag != "" {
+		env.Branch = *GitBranchFlag
+	}
+	if *GitTagFlag != "" {
+		env.Tag = *GitTagFlag
+	}
+	if *BuildnumFlag != "" {
+		env.Buildnum = *BuildnumFlag
+	}
+	if *PullRequestFlag {
+		env.IsPullRequest = true
+	}
+	if *CronJobFlag {
+		env.IsCronJob = true
+	}
+	return env
 }
